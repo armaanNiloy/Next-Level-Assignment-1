63f89b48ce6d9a18a98383f3064626dab651f496
ArmaanNiloy <niloy.barca@outlook.com>
2026-05-07 00:03:04 +0600

    Problem 1,2,3,4

---
+++ solutions.ts
@@ -0,0 +1,39 @@
+//problem 1
+
+function filterEvenNumbers(num: number[]): number[]{
+    return num.filter((value)=>value%2===0);
+}
+
+//problem 2
+
+function reverseString(input: string): string{
+    let returnValue = "";
+    for(let i = input.length -1; i>=0 ; i--){
+        returnValue+= input[i];
+    }
+    return returnValue;
+}
+
+//problem 3
+
+type StringOrNumber = string | number;
+
+function checkType(input : StringOrNumber){
+    if(typeof input === 'string')
+        return "String"
+    else if(typeof input == "number")
+        return "number";
+}
+
+// Problem 4
+
+const user = { id: 1, name: "John Doe", age: 21 };
+
+function getProperty<T>(obj: T, key: keyof T) {
+  return obj[key];
+}
+
+// Problem 5
+
+
+
